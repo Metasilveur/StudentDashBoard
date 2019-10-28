73dcb37e445064fe97d2c71e5a5de1a561fad9bb
Robert <robert.bui@outlook.fr>
2019-10-28 16:13:27 +0100

    Add grades tables

--- Front/src/Dashboard4.js
+++ Front/src/Dashboard4.js
@@ -1,182 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { Link as RouterLink, withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
 import {
-  Grid,
-  Button,
-  IconButton,
-  TextField,
-  Link,
-  Typography
+  Card,
+  CardHeader,
+  CardContent
 } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
-const schema = {
-  email: {
-    presence: { allowEmpty: false, message: 'is required' },
-    email: true,
-    length: {
-      maximum: 64
-    }
-  },
-  password: {
-    presence: { allowEmpty: false, message: 'is required' },
-    length: {
-      maximum: 128
-    }
-  }
-};
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   root: {
-    backgroundColor: '#FFFFFF',
-    height: '100%'
+    width: '100%',
+    overflowX: 'auto',
   },
-  grid: {
-    height: '100%'
+  table: {
+    minWidth: 650,
   },
-  quote: {
-    backgroundColor: '#FFFFFF',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundImage: 'url(/images/auth.jpg)',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center'
-  },
-  quoteInner: {
-    textAlign: 'center',
-    flexBasis: '600px'
-  },
-  quoteText: {
-    color: '#FFFFFF',
-    fontWeight: 300
-  },
-  name: {
-    marginTop: theme.spacing(3),
-    color: '#FFFFFF'
-  },
-  bio: {
-    color: '#FFFFFF'
-  },
-  contentContainer: {},
-  content: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  contentHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    paddingTop: theme.spacing(5),
-    paddingBototm: theme.spacing(2),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2)
-  },
-  logoImage: {
-    marginLeft: theme.spacing(4)
-  },
-  contentBody: {
-    flexGrow: 1,
-    display: 'flex',
-    alignItems: 'center',
-    [theme.breakpoints.down('md')]: {
-      justifyContent: 'center'
-    }
-  },
-  form: {
-    paddingLeft: 100,
-    paddingRight: 100,
-    paddingBottom: 125,
-    flexBasis: 700,
-    [theme.breakpoints.down('sm')]: {
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(2)
-    }
-  },
-  title: {
-    marginTop: theme.spacing(3)
-  },
-  socialButtons: {
-    marginTop: theme.spacing(3)
-  },
-  socialIcon: {
-    marginRight: theme.spacing(1)
-  },
-  sugestion: {
-    marginTop: theme.spacing(2)
-  },
-  textField: {
-    marginTop: theme.spacing(2)
-  },
-  signInButton: {
-    margin: theme.spacing(2, 0)
-  }
-}));
+});
+
+const rows = [
+  { matiere:'Matière 1', bloc:'Bloc 1', note:10},
+  { matiere:'Matière 2', bloc:'Bloc 2', note:10},
+  { matiere:'Matière 3', bloc:'Bloc 2', note:10},
+  { matiere:'Matière 4', bloc:'Bloc 3', note:10},
+  { matiere:'Matière 5', bloc:'Bloc 2', note:10},
+  { matiere:'Matière 6', bloc:'Bloc 2', note:10},
+  { matiere:'Matière 7', bloc:'Bloc 1', note:10},
+  { matiere:'Matière 8', bloc:'Bloc 2', note:10},
+]
 
 const Dashboard4 = props => {
-  const { history } = props;
-
   const classes = useStyles();
-
-  const [formState, setFormState] = useState({
-    isValid: false,
-    values: {},
-    touched: {},
-    errors: {}
-  });
-
-  useEffect(() => {
-    const errors = false;
-
-    setFormState(formState => ({
-      ...formState,
-      isValid: errors ? false : true,
-      errors: errors || {}
-    }));
-  }, [formState.values]);
-
-  const handleBack = () => {
-    history.goBack();
-  };
-
-  const handleChange = event => {
-    event.persist();
-
-    setFormState(formState => ({
-      ...formState,
-      values: {
-        ...formState.values,
-        [event.target.name]:
-          event.target.type === 'checkbox'
-            ? event.target.checked
-            : event.target.value
-      },
-      touched: {
-        ...formState.touched,
-        [event.target.name]: true
-      }
-    }));
-  };
-
-  const handleSignIn = event => {
-    event.preventDefault();
-    history.push('/');
-  };
-
-  const hasError = field =>
-    formState.touched[field] && formState.errors[field] ? true : false;
-
   return (
-    <div className={classes.root}>
-    </div>
-  );
-};
+    <Grid>
+      <Grid item xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Simple Table</h4>
+            <p className={classes.cardCategoryWhite}>
+              Here is a subtitle for this table
+            </p>
+          </CardHeader>
+          <CardContent>
 
-Dashboard4.propTypes = {
-  history: PropTypes.object
-};
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                <TableCell >Matière</TableCell>
+                <TableCell align="left">Bloc</TableCell>
+                <TableCell align="left">Note</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map(row => (
+                <TableRow key={row.matiere}>
+                  <TableCell >
+                    {row.matiere}
+                  </TableCell>
+                  <TableCell align="left">{row.bloc}</TableCell>
+                  <TableCell align="left">{row.note}</TableCell>
+                </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+}
+
 
 export default Dashboard4;
